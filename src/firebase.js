bb03e66987a90a51e2830d13e7fb769542cd2db4
Gustavo <gugars04@gmail.com>
2026-07-31 17:04:46 -0300

    Movendo chaves para arquivo .env

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // SUBSTITUA ESTE BLOCO PELO CÓDIGO QUE VOCÊ COPIOU DO FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyBvPL_X1kJ7kWNxOXTRLUSJrvP8qNwU3ss",
-  authDomain: "sistema-de-doces.firebaseapp.com",
-  projectId: "sistema-de-doces",
-  storageBucket: "sistema-de-doces.firebasestorage.app",
-  messagingSenderId: "937659928220",
-  appId: "1:937659928220:web:7351cfe2cd83aa88ada759"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Inicializa o aplicativo do Firebase
